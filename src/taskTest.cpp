498dd677ac9ba52cc45c09a651692fb2379be445
Fidifis <fdfswork@seznam.cz>
2022-04-02 12:09:43 +0200

    Remove forgotten call

--- src/taskTest.cpp
+++ src/taskTest.cpp
@@ -9,9 +9,8 @@ TaskTest::TaskTest(const Task *task, const string &testName, const nlohmann::jso
     substituteAllNames();
 }
 
-void TaskTest::SpecialLoad(stringRef& item, const string& jsonValue) {
-    Task::SpecialLoad(item, jsonValue);
-
+void TaskTest::SpecialLoad(stringRef& item, const string& jsonValue)
+{
     if (item.key == PARAM.compileArgs.key)
     {
         if (PARAM.compileArgs.value != jsonValue)
