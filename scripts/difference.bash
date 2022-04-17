3e3fd16db583619959da420ca98f4785bee193e4
Fidifis <fdfswork@seznam.cz>
2022-04-17 15:37:15 +0200

    Remove grep from script

--- scripts/difference.bash
+++ scripts/difference.bash
@@ -3,9 +3,5 @@ OUTPUT="$1";
 REFERENCE="$2";
 DIFF_FILE="$3";
 
-diff -y --width=50 "$OUTPUT" "$REFERENCE" > "$DIFF_FILE"'.tmp';
-EXIT_CODE="$?";
-
-grep -v "^---" "$DIFF_FILE"'.tmp' > "$DIFF_FILE";
-rm "$DIFF_FILE"'.tmp';
-exit "$EXIT_CODE";
+diff -y --width=50 "$OUTPUT" "$REFERENCE" > "$DIFF_FILE";
+exit "$?";
